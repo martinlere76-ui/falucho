38b4e2d42c758275aeaf057dfaf01e1f23ec2ca8
martinlere76-ui <martinlere76@gmail.com>
2026-05-21 17:57:53 -0300

    Delete gyver_stringutils_link.cpp

--- gyver_stringutils_link.cpp
+++
@@ -1,8 +0,0 @@
-// Link Gyver StringUtils translation units when the local shim is used.
-#include "C:/Users/Martin/Documents/Arduino/libraries/StringUtils/src/utils/hash.cpp"
-#include "C:/Users/Martin/Documents/Arduino/libraries/StringUtils/src/utils/list.cpp"
-#include "C:/Users/Martin/Documents/Arduino/libraries/StringUtils/src/utils/convert/b64.cpp"
-#include "C:/Users/Martin/Documents/Arduino/libraries/StringUtils/src/utils/convert/convert.cpp"
-#include "C:/Users/Martin/Documents/Arduino/libraries/StringUtils/src/utils/convert/qwerty.cpp"
-#include "C:/Users/Martin/Documents/Arduino/libraries/StringUtils/src/utils/convert/unicode.cpp"
-#include "C:/Users/Martin/Documents/Arduino/libraries/StringUtils/src/utils/convert/url.cpp"
